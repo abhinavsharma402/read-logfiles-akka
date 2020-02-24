d1df403fcc0020de2111b310cd0d5c1e2bf1fe0b
abhinavsharma402 <abhinavsharma402@gmail.com>
2020-02-24 11:14:35 +0530

    update source file

--- src/main/scala/FileHAndlling.scala
+++ src/main/scala/FileHAndlling.scala
@@ -14,13 +14,23 @@ class CheckFile extends Actor {
   override def receive: Receive = {
     case "checkFilesOrFoldersList" =>
       val files1 = checkFile(FileHandling.filesOrFoldersList)
+
       sender() ! files1
-    case "readFile"=>
-      val contents=readFile(FileHandling.filesList)
-      sender()!contents
-    case "readError"=>
-      val error=readError(FileHandling.content)
-      sender()!error
+    case "readFile" =>
+      val contents = readFile(FileHandling.filesList)
+      sender() ! contents
+    case "readError" =>
+      val error = readError(FileHandling.content)
+      sender() ! error
+    case "readWarnings" =>
+      val warnings = readWarnings(FileHandling.content)
+      sender() ! warnings
+    case "readInfo" =>
+      val info = readInfo(FileHandling.content)
+      sender() ! info
+    case "averageWarnings" =>
+      val warnings = averageWarnings(FileHandling.warnings)
+      sender() ! warnings
 
 
   }
@@ -65,15 +75,20 @@ object FileHandling extends App {
   val ref = system.actorOf(Props[CheckFile], "FileHandling")
   val pathObj = new File("/home/knoldus/Downloads/logfiles")
   val filesOrFoldersList = pathObj.listFiles().toList
-  implicit val timeout = Timeout(30. seconds)
-  val futureFilesList = (ref ? "filesOrFoldersList").mapTo[List[File]]
+  implicit val timeout = Timeout(5. seconds)
+  val futureFilesList = (ref ? "checkFilesOrFoldersList").mapTo[List[File]]
   val filesList = Await.result(futureFilesList, 15.seconds)
-  val futureContent=(ref ? "readFile").mapTo[List[(String,List[String])]]
-  val content=Await.result(futureContent,5.seconds)
- val futureError= (ref ? "readError").mapTo[List[(String,Int)]]
-  val error=(Await.result(futureError,5.seconds))
-  for(d<-error){
-    println(d)
+  val futureContent = (ref ? "readFile").mapTo[List[(String, List[String])]]
+  val content = Await.result(futureContent, 5.seconds)
+  val futureError = (ref ? "readError").mapTo[List[(String, Int)]]
+  val error = (Await.result(futureError, 5.seconds))
+  val futureWarnings = (ref ? "readWarnings").mapTo[List[(String, Int)]]
+  val warnings = (Await.result(futureWarnings, 5.seconds))
+  val futureInfo = (ref ? "readInfo").mapTo[List[(String, Int)]]
+  val info = (Await.result(futureInfo, 5.seconds))
+  val futureaverageWarnings = (ref ? "averageWarnings").mapTo[Int]
+  val averagewarnings = (Await.result(futureaverageWarnings, 5.seconds))
+  println(averagewarnings)
 
-  }
+
 }
